dd0d259085019893a04750aa03a5051d2cf7d0f5
Gliskle <168364962+Gliskle@users.noreply.github.com>
2026-03-31 07:38:48 -0700

    phaser button

--- main.js
+++ main.js
@@ -24,6 +24,40 @@ function create ()
     // for (let i = 0; i < cards.length; i++) {
     //    this.add.text(0, lineHeight * lines++, cards[i].title);
     // }
+    class Example extends Phaser.Scene
+    {
+        create ()
+        {
+            const button = this.add.text(400, 300, 'Play Game', {
+                fontSize: '32px',
+                color: '#ffffff',
+                align: 'center',
+                fixedWidth: 260,
+                backgroundColor: '#2d2d2d',
+                borderRadius: '8px'
+            }).setPadding(32).setOrigin(0.5);
+
+            button.setInteractive({ useHandCursor: true });
+
+            button.on('pointerover', () => {
+            button.setBackgroundColor('#8d8d8d');
+            });
+
+            button.on('pointerout', () => {
+                button.setBackgroundColor('#2d2d2d');
+            });
+        }
+    }
+
+    const config = {
+        type: Phaser.AUTO,
+        parent: 'phaser-example',
+        width: 800,
+        height: 600,
+        scene: Example
+    };
+
+    const game = new Phaser.Game(config);
 }
 
 function update ()
