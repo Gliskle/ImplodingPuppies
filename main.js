f73c7000d4adb0b4ef86fa12ebe79b53f2a4e06d
Gliskle <168364962+Gliskle@users.noreply.github.com>
2026-04-02 17:17:27 +0000

    turn

--- main.js
+++ main.js
@@ -52,6 +52,47 @@ function create ()
         this.startButton.setBackgroundColor("#8d8d8d")
         newGame = true;
     });
+
+    this.drawPile = this.add.text(960, 540, 'Pick Up Card', {
+        fontSize: '25px',
+        color: '#ffffff',
+        align: 'center',
+        fixedWidth: 260,
+        backgroundColor: '#2d2d2d',
+    }).setPadding(32).setOrigin(0.5);
+
+    this.drawPile.setVisible(false)
+    this.drawPile.setInteractive({ useHandCursor: true});
+
+    this.drawPile.on('pointerover', () => {
+        this.drawPile.setBackgroundColor('#8d8d8d')
+    });
+
+    this.drawPile.on('pointerout', () => {
+        this.drawPile.setBackgroundColor('#2d2d2d')
+    });
+
+    this.drawPile.on('pointerdown', () => {
+        this.drawPile.setBackgroundColor('#4d4d4d')
+    });
+
+    this.drawPile.on('pointerup', () => {
+        this.drawPile.setBackgroundColor('#8d8d8d')
+    });
+
+    const player1 = this.player1 = this.add.text(1650, 25, 'Player1 (YOU)', {
+        fontSize: '30px',
+        color: '#ffffff',
+        align: 'center',
+    });
+    player1.setVisible(false)
+
+    const player2 = this.player2 = this.add.text(1650, 83, 'Player2', {
+        fontSize: '30px',
+        color: '#ffffff',
+        align: 'center',
+    });
+    player2.setVisible(false)
 }
 
 function update ()
@@ -62,6 +103,9 @@ function update ()
         this.startButton.setPosition(0,0)
         this.startButton.setOrigin(0,0)
         this.startButton.setText("New Game")
+        this.drawPile.setVisible(true)
+        this.player1.setVisible(true)
+        this.player2.setVisible(true)
 
         const { hands, deck } = dealAndReshuffle(cards)
 
@@ -72,38 +116,25 @@ function update ()
         console.log( { player1Hand, player2Hand, currentDeck })
         console.log(currentDeck[0])
 
-        const drawPile = this.drawPile = this.add.text(960, 540, 'Pick Up Card', {
-            fontSize: '25px',
-            color: '#ffffff',
-            align: 'center',
-            fixedWidth: 260,
-            backgroundColor: '#2d2d2d'
-        }).setPadding(32).setOrigin(0.5);
+        if (turn == "player1"){
+            this.player1.setColor("#8de8ff")
+        }
+        else {
+            this.player1.setColor("#ffffff")
+        }
 
-        this.drawPile.setInteractive({ useHandCursor: true});
+        
 
-        this.drawPile.on('pointerover', () => {
-            this.drawPile.setBackgroundColor('#8d8d8d')
-        });
-
-        this.drawPile.on('pointerout', () => {
-            this.drawPile.setBackgroundColor('#2d2d2d')
-        });
-
-        this.drawPile.on('pointerdown', () => {
-            this.drawPile.setBackgroundColor('#4d4d4d')
-        });
-
-        this.drawPile.on('pointerup', () => {
-            this.drawPile.setBackgroundColor('#8d8d8d')
-        });
-
-        const player1Turn = this.player1Turn = this.add.text(1775, 30, 'Player1 (YOU)', {
-            fontSize: '30px',
-            color: '#ffffff',
-            align: 'center'
-        }).setOrigin(0.5);
+        if (turn == "player2"){
+            this.player2.setColor("#8de8ff")
+        }
+        else {
+            this.player2.setColor("#ffffff")
+        }
     }
+
+    this.player1.setText(`Player1 (YOU)\n${player1Hand.length} Cards`)
+    this.player2.setText(`Player2      \n${player2Hand.length} Cards`)
 }
 
 
